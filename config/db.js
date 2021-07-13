464a3b6031bbfbcefa56658aa27f8de6d4531b8b
Miguel <miguelbermell@gmail.com>
2021-07-13 11:23:35 +0200

    db config

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require("sequelize");
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const dbConnection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "mysql",
+});
+
+module.exports = dbConnection;
